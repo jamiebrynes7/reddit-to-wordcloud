77ac8001b41677063279c7f38ade74b85dd33151
Jamie Brynes <jamiebrynes7@gmail.com>
2018-03-20 00:04:31 +0000

    lambda handler fixes

--- aws_lambda_service.py
+++ aws_lambda_service.py
@@ -1,7 +1,8 @@
 import json
 import base64
 from io import BytesIO
-
+import matplotlib
+matplotlib.use('Agg')
 from common_lib import generate_wordcloud, WordcloudSettings
 
 
@@ -9,14 +10,14 @@ ERROR_STRING = "ERROR: {0}"
 
 def handler(event, context):
 
-    wordcloud_settings = WordcloudSettings(**event["wordcloud_settings"]).add_url(event["url"])
+    body = json.loads(event['body'])
+    wordcloud_settings = WordcloudSettings(**body["wordcloud_settings"]).add_url(body["url"])
 
     try:
         image = generate_wordcloud(wordcloud_settings)
         buffer = BytesIO()
         image.save(buffer, format="JPEG")
-        img_str = base64.b64encode(buffer.getvalue())
+        img_str = base64.b64encode(buffer.getvalue()).decode()
         return {"image": img_str}
     except Exception as e:
         return {"error": ERROR_STRING.format("Failed to generate wordcloud with error " + str(e))}
-      
